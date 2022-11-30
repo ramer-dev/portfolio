4ac604162ddb071f8deabd8d10f875b8c635a3ce
Ramon K. Shin <gml4009@gmail.com>
2022-11-30 21:56:33 +0900

    Tailwind CSS Template Literal 에러

--- components/header/TopNavigationBar.tsx
+++ components/header/TopNavigationBar.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
 export default function TopNavigationBar(): JSX.Element {
+    const onClickHandler = () => {
+        console.log('click')
+    } 
+    
     return (
-        <div>탑바
+        <div className=" h-12 flex justify-between px-40">
+            <div className="my-3 text-xs"> Logo </div>
+            <div className="flex gap-4">
+                <span onClick={onClickHandler}>test</span>
+                <span>test</span>
+                <span>test</span>
+
+            </div>
         </div>
     )
 }
