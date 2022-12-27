2cde6e525e94aa09dd0eca7a509136082e03c7c3
Ramon K. Shin <gml4009@gmail.com>
2022-12-27 16:19:40 +0900

    Particle System Applied

--- components/page/intro/intro.tsx
+++ components/page/intro/intro.tsx
@@ -1,30 +1,61 @@
-import React, {RefObject, useEffect, useRef, useState} from "react";
-import {useRecoilValue} from "recoil";
+import React, {BaseSyntheticEvent, RefObject, useEffect, useRef, useState} from "react";
 import {globalWidthState} from "../../../lib/states/states";
 import {useCanvas} from "../../../lib/hooks/useCanvas";
 import {useClientWidthHeight} from "../../../lib/hooks/useClientWidthHeight";
-import { ILightParticle, LightParticle } from "../../../pages/canvas/LightParticle";
+import {ILightParticle, LightParticle} from "../../../pages/canvas/LightParticle";
+import Flat from "./particle/Flat";
 
 const Intro: React.FC = () => {
 
-    const mainRef = useRef<HTMLDivElement>(null);
+    const mainRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const parRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
     const rect = useClientWidthHeight(mainRef)
+    const [isHover, setHover] = useState(false);
 
-    const fillBackground = (ctx : CanvasRenderingContext2D) => {
+    const fillBackground = (ctx: CanvasRenderingContext2D) => {
         ctx.fillStyle = 'rgb(31,31,36)';
-        ctx.fillRect(0,0, rect.width, rect.height)
+        ctx.fillRect(0, 0, rect.width, rect.height)
     }
 
-    const lightParticle: ILightParticle = new LightParticle(rect.width/2, rect.height/2)
+    const lightParticle: ILightParticle = new LightParticle(rect.width / 2, rect.height / 2)
     const animate = (ctx: CanvasRenderingContext2D) => {
         fillBackground(ctx);
         lightParticle.drawLightSource(ctx, 50, 50);
     }
 
+
+    const eventHandler = (e: BaseSyntheticEvent) => {
+        switch (e.type) {
+            case 'mouseenter':
+                setHover(true);
+                break;
+
+            case 'mouseleave':
+                setHover(false);
+                break;
+        }
+    }
+
+    // useEffect(() => {
+    //     if(isHover){
+    //
+    //     }
+    // }, [isHover])
+
     const canvasRef = useCanvas(rect.width, rect.height, animate)
 
-    return <div className={'w-full h-screen'} ref={mainRef}>
-        <canvas className={'h-full m-0 p-0'} ref={canvasRef}/>
+    return <div className={'w-full h-screen bg-neutral-900 overflow-hidden'} ref={mainRef}>
+        <Flat height={400} width={200} left={100} top={200}/>
+        <Flat height={450} width={150} left={50} top={250}/>
+        <Flat height={200} width={100} left={0} top={380}/>
+
+        <Flat height={450} width={150} left={900} top={350}/>
+        <Flat height={200} width={100} left={800} top={380}/>
+
+        <div className={'relative top-1/2 mx-24'} ref={parRef} onMouseEnter={eventHandler} onMouseLeave={eventHandler}>
+            <h1 className={'text-8xl text-white font-bold hover:opacity-80 transition cursor-default'}>HS Shin</h1>
+            <p className={'mx-1 text-xl text-white hover:opacity-80 transition cursor-default'}>FE Developer</p>
+        </div>
     </div>
 }
 export default Intro;
--- components/page/intro/particle/Flat.tsx
+++ components/page/intro/particle/Flat.tsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useRef } from "react";
-import { AnimatePresence, motion, useMotionValue, useSpring } from "framer-motion";
-import { distance } from "popmotion";
+import React, {useEffect, useRef, useState} from "react";
+import {AnimatePresence, motion, useMotionValue, useSpring, Variants} from "framer-motion";
+import {distance} from "popmotion";
 // import './flat_particle.css';
 
 type ParticleType = {
     width: number,
     height: number,
-    zDepth: number,
-    // left:number,
-    // top:number
+
+    left: number,
+    top: number
 }
 
 export default function Flat(props: ParticleType): JSX.Element {
     const x = useMotionValue(0);
     const spring = useSpring(x);
 
-    return (
-            <AnimatePresence mode='wait'>
-                <motion.div className={`grid-item relative bg-slate-600 rounded-full mx-auto`}
-                    initial={{y:100}}
-                    whileInView={"visible"}
-                    viewport={{once:true}}
-                    animate={{ y:[10,-10, 10,], }}
-                    transition={{ duration:3, ease: "easeInOut", times:[0,0.5, 1], repeat:Infinity, repeatDelay:0}}
-                    style={{width:props.width, height:props.height}}
-                >
-                </motion.div>
 
-            </AnimatePresence>
+    return (
+        <AnimatePresence mode='wait'>
+            <motion.div className={`absolute rounded-full`}
+                        initial={{rotateZ: -30, opacity: .6}}
+                        whileHover={{opacity: 0.1}}
+                        transition={{
+                            duration: .25,
+                            ease: "easeInOut",
+                            times: [0, 0.5, 1],
+                        }}
+                        style={{
+                            width: props.width, height: props.height, left: props.left, top: props.top,
+                            background: "linear-gradient(180deg, rgba(217, 217, 217, 0) 8.33%, rgba(217, 217, 217, 0.43) 100%)"
+                        }}
+            >
+            </motion.div>
+
+        </AnimatePresence>
     )
 }
